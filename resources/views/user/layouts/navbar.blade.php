adf10f3ebb5befbfe73fdb510ce68612f5b96eb3
arvandha121 <nauvan121@gmail.com>
2022-06-25 11:09:07 +0700

    fix navbar user

--- resources/views/user/layouts/navbar.blade.php
+++ resources/views/user/layouts/navbar.blade.php
@@ -1,13 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand px-3" href="/user/home">
-        <img src="{{asset('assets/img/logo.png')}}" width="60" height="60" class="d-inline-block align-top" alt="">
-    </a>
+    <div class="container">
+        <a class="navbar-brand px-3" href="/user/home">
+            {{-- <img src="{{asset('assets/img/logo.png')}}" width="60" height="60" class="d-inline-block align-top" alt=""> --}}
+            <span class="brand-text font-weight-light">TokoSepatu.Com</span>
+        </a>
+    </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <a class="nav-item nav-link" href="/">Produk</span></a>
+            <a class="nav-item nav-link disabled active" href="/user/home">Produk</span></a>
             <!-- <a class="nav-item nav-link" href="/logout">Logout</a> -->
             <a class="nav-item nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
