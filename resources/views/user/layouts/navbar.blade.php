3255812937e2ecab41abb5e949ae08d281faaeec
arvandha121 <nauvan121@gmail.com>
2022-06-25 14:56:54 +0700

    fix navbar user2

--- resources/views/user/layouts/navbar.blade.php
+++ resources/views/user/layouts/navbar.blade.php
@@ -4,22 +4,22 @@
             {{-- <img src="{{asset('assets/img/logo.png')}}" width="60" height="60" class="d-inline-block align-top" alt=""> --}}
             <span class="brand-text font-weight-light">TokoSepatu.Com</span>
         </a>
-    </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link disabled active" href="/user/home">Produk</span></a>
-            <!-- <a class="nav-item nav-link" href="/logout">Logout</a> -->
-            <a class="nav-item nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                <i class="bi bi-box-arrow-left"></i> {{ __('Logout') }}
-            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ">
+                <a class="nav-item nav-link disabled active" href="/user/home">Produk</span></a>
+                <!-- <a class="nav-item nav-link" href="/logout">Logout</a> -->
+                <a class="nav-item nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                    <i class="bi bi-box-arrow-left"></i> {{ __('Logout') }}
+                </a>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
         </div>
     </div>
 </nav>
