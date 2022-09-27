9f4c34bcca50277a6b37d4dd42b269c79928db0e
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2022-09-27 22:20:56 +0700

    Blackbox Testing

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -28,7 +28,7 @@
   <!-- summernote -->
   <link rel="stylesheet" href="{{asset('assets/plugins/summernote/summernote-bs4.min.css')}}">
 
-   
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -46,12 +46,7 @@
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <!-- <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Dashboard</a>
-        </li> -->
-        <!-- <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Contact</a>
-        </li> -->
+
       </ul>
 
       <!-- Right navbar links -->
@@ -177,29 +172,7 @@
     </nav>
     <!-- /.navbar -->
 
-    <!-- Content Wrapper. Contains page content -->
-    <!-- <div class="content-wrapper"> -->
-    <!-- Content Header (Page header) -->
-    <!-- <div class="content-header"> -->
-    <!-- <div class="container-fluid"> -->
-    <!-- <div class="row mb-2"> -->
-    <!-- <div class="col-sm-6"> -->
-    <!-- <h1 class="m-0">Dashboard</h1> -->
-    <!-- </div> -->
-    <!-- /.col -->
-    <!-- <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div> -->
-    <!-- /.col -->
-    <!-- </div> -->
-    <!-- /.row -->
-    <!-- </div> -->
-    <!-- /.container-fluid -->
-    <!-- </div> -->
-    <!-- /.content-header -->
+
 
     <!-- Main content -->
     @yield('dashboard')
